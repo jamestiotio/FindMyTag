b62d61676eb2caef74dbef2d63ff31a912ea70c9
James R T <jamestiotio@gmail.com>
2021-03-24 01:02:18 +0800

    Add measured altitude constants as data class

---
+++ app/src/main/java/com/example/findmytag/algorithms/neuralnetwork/AltitudeConstants.java
@@ -0,0 +1,17 @@
+// package com.example.findmytag.algorithms.neuralnetwork;
+
+/**
+ * Define experimentally-measured average altitude constants (units in metres; measured from floor
+ * of first level). This assumes that every user/device has the same height and every AP at each
+ * floor is at the same constant altitude.
+ */
+public final class AltitudeConstants {
+    public static final double USER_FIRST_LEVEL_ALTITUDE = 1.35;
+    public static final double USER_SECOND_LEVEL_ALTITUDE = 5.85;
+    public static final double AP_FIRST_LEVEL_ALTITUDE = 4.20;
+    public static final double AP_SECOND_LEVEL_ALTITUDE = 7.25;
+
+    public AltitudeConstants() {
+
+    }
+}
