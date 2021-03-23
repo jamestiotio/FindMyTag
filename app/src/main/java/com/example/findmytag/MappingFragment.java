bdafa3f03237089d329a675ea90acde43b7fc6fd
Darren Loh <vanhfdarren@gmail.com>
2021-03-23 18:13:01 +0800

    Added uploading popup to include names and pictures

--- app/src/main/java/com/example/findmytag/MappingFragment.java
+++ app/src/main/java/com/example/findmytag/MappingFragment.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.PointF;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -22,6 +23,8 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
+
+import java.net.URI;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -80,8 +83,9 @@ public class MappingFragment extends Fragment {
     //outside
     Button upload_btn;
     Marker mapping_floorplan_imgView;
-    private static final int IMAGE_PICK_CODE = 1000;
-    private static final int PERMISSION_CODE = 1001;
+    private final int activity_code = 2000;
+
+
     private boolean ready = false; // boolean to check if user uploaded image
     //---
 
@@ -124,51 +128,14 @@ public class MappingFragment extends Fragment {
         });
         //--------Marker touch event------------
 
-
-        // ---------Upload image--------------
+        //------upload btn---------
         upload_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                //check runtime permission
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                    if(getActivity().checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
-                        //permission not granted, request it.
-                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
-                        //show popup for permission
-                        requestPermissions(permissions, PERMISSION_CODE);
-
-                    }
-                    else {
-                        //permission already granted
-                        pickImageFromGallery();
-                    }
-                }
-                else{
-                    pickImageFromGallery();
-                }
+                startActivityForResult(new Intent(getContext(),imgPopupWindow.class),activity_code);
             }
         });
-    }
-
-    private void pickImageFromGallery() {
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent, IMAGE_PICK_CODE);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
-            case PERMISSION_CODE:
-                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    pickImageFromGallery();
-                }
-                else{
-                    Toast.makeText(getContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
-                }
-        }
+        //------upload btn---------
 
 
     }
@@ -176,14 +143,13 @@ public class MappingFragment extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
-            //set image to image view
-            //mapping_floorplan_imgView.setImageURI(data.getData());
-            mapping_floorplan_imgView.setImage(ImageSource.uri(data.getData()));
-            ready = true;
+        if(requestCode == activity_code){
+            if (resultCode == Activity.RESULT_OK){
+                String location_name = data.getStringExtra("locationName");
+                Uri imgUri = Uri.parse(data.getExtras().getString("imgUri"));
+                mapping_floorplan_imgView.setImage(ImageSource.uri(imgUri));
+                ready = true;
+            }
         }
     }
-
-    //---------upload img-----------
-
 }
