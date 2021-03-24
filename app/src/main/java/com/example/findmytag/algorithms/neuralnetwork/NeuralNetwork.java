79e88eeae940a39247e90f92cb3f9a9a7c5a0fc5
James R T <jamestiotio@gmail.com>
2021-03-24 16:01:03 +0800

    Update neural network description

--- app/src/main/java/com/example/findmytag/algorithms/neuralnetwork/NeuralNetwork.java
+++ app/src/main/java/com/example/findmytag/algorithms/neuralnetwork/NeuralNetwork.java
@@ -1,7 +1,15 @@
 // package com.example.findmytag.algorithms.neuralnetwork;
 
 /**
- * This is just a simple feed-forward neural network, nothing fancy.
+ * This is just a simple feed-forward neural network, nothing fancy. This network uses RSSI instead
+ * of CSI information data. Based on this research paper:
+ * https://ieeexplore.ieee.org/document/8662548, using CSI data will result in relatively better and
+ * more accurate localization results. However, no current smartphone API allows access to the Wi-Fi
+ * AP's physical layer CSI information (this includes the Android API). Some level of hacking to the
+ * lower layers could be executed but its legality is questionable. Manual offline mapping using the
+ * Linux 802.11n CSI Tool (https://dhalperi.github.io/linux-80211n-csitool/) could be conducted but
+ * this is not entirely in line with the project's requirements to actually conduct the mapping
+ * on-the-fly on the smartphone mobile device itself.
  */
 public class NeuralNetwork {
 
