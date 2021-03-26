93623992e42d05d16f9b18b7654421b48e3fd3a8
James R T <jamestiotio@gmail.com>
2021-03-26 11:08:03 +0800

    Add hardcoded SUTD Building 2 MAC Addresses Wi-Fi AP List

--- app/src/main/java/com/example/findmytag/algorithms/neuralnetwork/NeuralNetwork.java
+++ app/src/main/java/com/example/findmytag/algorithms/neuralnetwork/NeuralNetwork.java
@@ -1,8 +1,11 @@
 // package com.example.findmytag.algorithms.neuralnetwork;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * This is just a simple feed-forward neural network, nothing fancy. This network uses RSSI instead
- * of CSI information data. Based on this research paper:
+ * This is just a simple feed-forward convolutional neural network (Conv-ReLu CNN-LOC), nothing
+ * fancy. This network uses RSSI instead of CSI information data. Based on this research paper:
  * https://ieeexplore.ieee.org/document/8662548, using CSI data will result in relatively better and
  * more accurate localization results. However, no current smartphone API allows access to the Wi-Fi
  * AP's physical layer CSI information (this includes the Android API). Some level of hacking to the
@@ -17,5 +20,18 @@
  * not generally scale to N floors (this is only for 2 floors).
  */
 public class NeuralNetwork {
-
+    private final List<String> KNOWN_B2L1_AP_LIST = Arrays.asList("18-64-72-CD-5F-EC",
+            "18-64-72-CD-5F-E6", "18-64-72-CD-60-00", "18-64-72-CD-5F-BE", "18-64-72-CD-5F-C6",
+            "18-64-72-CD-60-88", "18-64-72-CD-60-9A", "18-64-72-CD-5F-B8", "18-64-72-CD-5F-38",
+            "18-64-72-CD-5F-F4", "18-64-72-CD-60-14", "18-64-72-CD-60-DE", "18-64-72-CD-60-4A",
+            "18-64-72-CD-60-D6", "18-64-72-CD-60-56", "18-64-72-CD-60-36", "18-64-72-CD-65-E6",
+            "18-64-72-CD-5F-8C", "18-64-72-CD-60-7A");
+    private final List<String> KNOWN_B2L2_AP_LIST = Arrays.asList("18-64-72-CD-5F-06",
+            "18-64-72-CD-60-AE", "18-64-72-CD-5F-C8", "18-64-72-CD-62-E0", "18-64-72-CD-60-8A",
+            "18-64-72-CD-62-3C", "18-64-72-CD-62-40", "18-64-72-CD-62-74", "18-64-72-CD-5F-0E",
+            "18-64-72-CD-53-48", "18-64-72-CD-64-34", "18-64-72-CD-65-C4", "18-64-72-CD-64-4C",
+            "18-64-72-CD-62-54", "18-64-72-CD-62-78", "18-64-72-CD-62-80", "18-64-72-CD-62-82",
+            "18-64-72-CD-61-38", "18-64-72-CD-53-A4", "18-64-72-CD-66-7C", "18-64-72-CD-52-AC",
+            "18-64-72-CD-53-70", "18-64-72-CD-52-BC", "18-64-72-CD-64-AA", "18-64-72-CD-65-A6",
+            "18-64-72-CD-66-8C", "18-64-72-CD-5F-54");
 }
