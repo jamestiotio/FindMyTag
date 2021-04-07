518a787ae661136ac9c5d789c07937e70a259289
zenibbles <65108129+zenibbles@users.noreply.github.com>
2021-04-07 23:06:40 +0800

    DataParser for all algos. Use this to get the components of the BSSID scans

---
+++ app/src/main/java/com/example/findmytag/DataParserAlgos.java
@@ -0,0 +1,21 @@
+package com.example.findmytag;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Use this to parse data and input into our algo
+ */
+public class DataParserAlgos {
+
+    public void parseSSID(HashMap map) {
+
+    }
+
+
+    public void parseXY() {
+
+    }
+}
