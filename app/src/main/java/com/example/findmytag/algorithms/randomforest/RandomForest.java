6805d7726140d66c91043923fcd96618f34400d5
zenibbles <65108129+zenibbles@users.noreply.github.com>
2021-04-17 16:52:11 +0800

    Update RandomForest.java

--- app/src/main/java/com/example/findmytag/algorithms/randomforest/RandomForest.java
+++ app/src/main/java/com/example/findmytag/algorithms/randomforest/RandomForest.java
@@ -1,8 +1,48 @@
-import tech.tablesaw.api.NumberColumn;
-import tech.tablesaw.util.DoubleArrays;
-
-import java.util.SortedSet;
-import java.util.TreeSet;
-
-public class RandomForest extends AbstractClassifier {
-
+//package com.example.findmytag.algorithms.randomforest;
+//
+//import com.example.findmytag.algorithms.randomforest.classification.AbstractClassifier;
+//
+//import com.example.findmytag.algorithms.randomforest.classification.ConfusionMatrix;
+//import com.example.findmytag.algorithms.randomforest.classification.IntConvertibleColumn;
+//import com.example.findmytag.algorithms.randomforest.classification.StandardConfusionMatrix;
+//import com.google.common.base.Preconditions;
+//
+//import tech.tablesaw.api.NumberColumn;
+//import tech.tablesaw.util.DoubleArrays;
+//
+//import java.util.SortedSet;
+//import java.util.TreeSet;
+//
+//public class RandomForest extends AbstractClassifier {
+//
+//    private final smile.classification.RandomForest classifierModel;
+//
+//    private RandomForest(int nTrees, int[] classArray, NumberColumn... columns) {
+//        double[][] data = DoubleArrays.to2dArray(columns);
+//        this.classifierModel = new smile.classification.RandomForest(data, classArray, nTrees);
+//    }
+//
+//    public static RandomForest learn(int nTrees, IntConvertibleColumn classes, NumberColumn... columns) {
+//        int[] classArray = classes.toIntArray();
+//        return new RandomForest(nTrees, classArray, columns);
+//    }
+//
+//    public int predict(double[] data) {
+//        return classifierModel.predict(data);
+//    }
+//
+//    public ConfusionMatrix predictMatrix(IntConvertibleColumn labels, NumberColumn... predictors) {
+//        Preconditions.checkArgument(predictors.length > 0);
+//
+//        SortedSet<Object> labelSet = new TreeSet<>(labels.asIntegerSet());
+//        ConfusionMatrix confusion = new StandardConfusionMatrix(labelSet);
+//
+//        populateMatrix(labels.toIntArray(), confusion, predictors);
+//        return confusion;
+//    }
+//
+//    @Override
+//    int predictFromModel(double[] data) {
+//        return classifierModel.predict(data);
+//    }
+//}
